57fbdf34604d90a66c412e941403c093d4ecc4a3
Veno <vtvelkov@gmail.com>
2018-03-16 12:17:03 +0200

    Update

--- common.php
+++ common.php
@@ -12,8 +12,12 @@ print "<script src=\"./js/bootstrap.min.js\"></script>";
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+	body{padding-top: 70px;}
+</style>
 
 </head>
+
 <script type="text/javascript" language="javascript">
 $(function() {
         $("#select-olt").change(function() {
