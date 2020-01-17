08abb40d001b7f10bb9ab7b7d2833802a8933eba
Charlie Root <vtvelkog@gmail.com>
2020-01-17 20:23:52 +0200

    modified:   classes/customers_class.php

--- classes/customers_class.php
+++ classes/customers_class.php
@@ -1061,12 +1061,12 @@ class customers {
 	
 	function onu_power_rrd() {
 		$rrd_name = dirname(dirname(__FILE__)) . "/rrd/" . $this->sn . "_power.rrd";
-		$opts = array( "--step", "900", "--start", "0",
-		   "DS:input:GAUGE:2700:U:U",
-		   "DS:output:GAUGE:2700:U:U",
-		   "DS:rxolt:GAUGE:2700:U:U",
-           "DS:rfin:GAUGE:2700:U:U",
-		   "RRA:AVERAGE:0.5:1:1800",
+		$opts = array( "--step", "300", "--start", "0",
+		   "DS:input:GAUGE:1800:U:U",
+		   "DS:output:GAUGE:1800:U:U",
+		   "DS:rxolt:GAUGE:1800:U:U",
+           "DS:rfin:GAUGE:1800:U:U",
+		   "RRA:AVERAGE:0.5:1:600",
 		   "RRA:AVERAGE:0.5:6:700",
 		   "RRA:AVERAGE:0.5:24:775",
 		   "RRA:AVERAGE:0.5:288:797",
