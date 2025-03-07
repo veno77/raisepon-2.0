bad144c4e5c5bd95f52af1c30c3860abf42b66a7
Veno <vtvelkov@gmail.com>
2025-03-07 12:44:33 +0200

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -3,7 +3,7 @@ $mysql_user = "raisepon";
 $mysql_pass = "r41sepon";
 global $db;
 try {
-	$db = new PDO('mysql:host=localhost;dbname=raisepon;charset=utf8', $mysql_user, $mysql_pass);
+	$db = new PDO('mysql:host=raisepon_db;dbname=raisepon;charset=utf8', $mysql_user, $mysql_pass);
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
 	echo 'Connection Failed: ' . $e->getMessage();
